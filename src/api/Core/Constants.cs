317a43f9ee6c7125a0247ff1e6e02e0d8d3918b2
Incredible <46843455+Incr3dible@users.noreply.github.com>
2020-07-16 14:22:18 +0200

    api

---
+++ src/api/Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace SupercellUilityApi.Core
+{
+    public class Constants
+    {
+        /// <summary>
+        /// The amount of hours the server waits until to check the server status again
+        /// </summary>
+        public const int ContentUpdateTimeout = 0;
+    }
+}
---
+++ src/api/Models/AssetFile.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SupercellUilityApi.Models
+{
+    public class AssetFile
+    {
+        [JsonPropertyName("sha")] public string Sha { get; set; }
+        [JsonPropertyName("file")] public string File { get; set; }
+    }
+}
